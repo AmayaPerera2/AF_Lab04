5c89f74429ced1f1a99b10aa7b2f35f3059a4ceb
Amaya Perera <pereradevindi14@gmail.com>
2026-02-22 10:48:51 +0530

    Added read file example

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello from SE3040 AF Lab 04!");
+//read file
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+ if (err) throw err;
+ console.log(data);
+});
